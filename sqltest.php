e26507ab449b82b7add7a1c2d71229c79508ec48
Woodyfree0 <132345685+Woodyfree0@users.noreply.github.com>
2023-09-04 02:06:04 +0000

    i hate this

--- sqltest.php
+++ sqltest.php
@@ -1,16 +1,26 @@
 <?php
-phpinfo();
-$servername = "localhost";
-$username = "username";
-$password = "password";
+# Fill our vars and run on cli
+# $ php -f db-connect-test.php
 
-// Create connection
-$conn = new mysqli($servername, $username, $password);
+$dbname = 'cafe';
+$dbuser = 'root';
+$dbpass = 'password';
+$dbhost = 'localhost';
 
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+$connect = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
+mysqli_select_db($connect, $dbname) or die("Could not open the db '$dbname'");
+
+$test_query = "SHOW TABLES FROM $dbname";
+$result = mysqli_query($connect, $test_query);
+
+$tblCnt = 0;
+while($tbl = mysqli_fetch_array($result)) {
+  $tblCnt++;
+  #echo $tbl[0]."<br />\n";
 }
-echo "Connected successfully";
-phpinfo();
-?>
+
+if (!$tblCnt) {
+  echo "There are no tables<br />\n";
+} else {
+  echo "There are $tblCnt tables<br />\n";
+}
