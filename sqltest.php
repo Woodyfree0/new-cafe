d58493f0bdd9204059e6aca4a0dfd9a8ea9113cc
Woodyfree0 <132345685+Woodyfree0@users.noreply.github.com>
2023-09-08 01:27:01 +0000

    do not mind

--- sqltest.php
+++ sqltest.php
@@ -4,21 +4,8 @@
 phpinfo();
 
 $serverName = "localhost"; // Replace with your SQL Server hostname or IP address
-$databaseName = "cafe"; // Replace with your database name
+$databaseName = "cafeDB"; // Replace with your database name
 $username = "root"; // Replace with your SQL Server username
 $password = "password"; // Replace with your SQL Server password
 
-try {
-    $dsn = "odbc:Driver={SQL Server};Server=$serverName;Database=$databaseName;";
-    $conn = new PDO($dsn, $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected to SQL Server successfully!";
-    
-    // You can perform database operations here
-    
-    // Don't forget to close the connection when done
-    $conn = null;
-} catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
-}
-?>
+
