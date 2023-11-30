c3c6c4f6bd30704325dc1298f77eb4d53cab8ae5
Woodyfree0 <132345685+Woodyfree0@users.noreply.github.com>
2023-11-30 00:10:02 +0000

    finished tests

--- src/tests/tests.php
+++ src/tests/tests.php
@@ -45,28 +45,7 @@ class RouterTests extends TestCase
 
         // Assert that the result is false
         $this->assertFalse($result);
-    }
-}
-class DBConnectTests extends TestCase
-{
-    public function testConnection()
-    {
-        $servername = "db";
-        $username = "php_docker";
-        $password = "password";
-        $database = "php_docker";
-
-        // Create an instance of the DB_Connect class
-        $dbConnect = new DB_Connect($servername, $username, $password, $database);
-
-        // Call the getConnection method and check if it returns a valid connection object
-        $connection = $dbConnect->getConnection();
-
-        // Assert that the connection object is an instance of mysqli
-        $this->assertInstanceOf(\mysqli::class, $connection, 'Database connection failed.');
-
-        // Optionally, close the connection when done
-        $dbConnect->closeConnection();
+        echo 'this route is not the same.';
     }
 }
 
